f20ba881f21877e46f2c08e5066cd0da61f902c0
shanboralkar <shantanuboralkar123@hotmail.com>
2019-12-12 10:16:27 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-u8da3q
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-u8da3q)
---
+++ index.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import Todo from './components/Todo';
+import './style.css';
+
+render(<Todo  />, document.getElementById('root'));
